1418af2325fcdd4915b96094bd27b8dc917e6063
Hussam812 <hossamhosnihussien@gmail.com>
2023-04-13 12:47:30 +0200

    sh13

---
+++ microverseLevelUpExperience/level2/sh13.js
@@ -0,0 +1,19 @@
+let num = 7;
+if(num !== 7){
+    console.log("Not seven");
+} else{
+    console.log("Lucky number seven!");
+};
+num = 10;
+if (num > 7){
+    console.log("Greater than seven");
+} else{
+    console.log("Smaller or equal")
+};
+
+num = 7 ;
+if (num >= 7 ){
+    console.log("Greater than or equal to seven");
+} else{
+    console.log("Smaller or equal")
+};
