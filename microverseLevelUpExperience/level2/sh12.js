e738f3ed7536092eff0aea20da5494c47086199e
Hussam812 <hossamhosnihussien@gmail.com>
2023-04-13 12:37:35 +0200

    excersie 2sheet12

---
+++ microverseLevelUpExperience/level2/sh12.js
@@ -0,0 +1,33 @@
+/*
+Exercise 2
+Please solve the following exercise befor moving ob:
+1- Create a varible 'X' with the value 32.
+2- Create an 'if' statement that detects if X is equle to 9.
+3- if the value is equle to 9 "console.log" the string "True"
+4- Otherwise, "console.log"the string "False"
+
+
+1- Create a varible "Y" with value "green".
+2- Create an "if" statement that detects if Y is equle to "Blue".
+3- If the value is equle to "Blue", "console.log" the string "Blue detected!"
+4- Create an 'else if' statement that detect if Y is equal to "Green"
+5- If the value is equle to "Green", "console.log" the string "Green detected!"
+6-Otherwise, "console.log" the string "No Green or Blue detected!"
+*/ 
+
+let X = 32;
+if (X == 9){
+    console.log("True");
+} else{
+    console.log("False");
+};
+
+let Y = "Green";
+if (Y == "Blue"){
+    console.log("Blue detected!");
+} else if (Y == "Green"){
+    console.log("Green detected!");
+} else{
+    console.log("No Green or Blue Detected!")
+}
+
