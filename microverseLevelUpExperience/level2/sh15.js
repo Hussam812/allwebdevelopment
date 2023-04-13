9c2ad2db54a2633764baa68af6eec170ec19b3ae
Hussam812 <hossamhosnihussien@gmail.com>
2023-04-13 12:56:52 +0200

    excercise3 sheet 15

---
+++ microverseLevelUpExperience/level2/sh15.js
@@ -0,0 +1,19 @@
+/*
+Excerise 3
+1- Create a varible "n" with the value 71.
+2- Create an "if" statement that detects if varible "n" is greater than 45.
+3- Inside that "if" statement, create another "if" statement that detects if
+the same varible is smaller than 65.
+4- Inside that "if" statement, add a "console.log" that shows the value of
+the varible "n".
+5- The value of "n" will not initially even if you hit "Run".
+6- Change the  value of "n" until it shows on terminal.
+*/
+
+let n = 71;
+n = 50
+if(n > 45){
+    if(n < 65){
+        console.log(n);
+    }
+};
