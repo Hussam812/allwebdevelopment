7887bf9cc7bcb005ed42e0db2da35bb014d73a48
Hussam812 <hossamhosnihussien@gmail.com>
2023-04-13 13:34:45 +0200

    exercise 6 sh21

---
+++ microverseLevelUpExperience/level2/sh21.js
@@ -0,0 +1,11 @@
+/*
+Exercise 6
+Please complete these steps befor moving to forward
+1- Create an array that contains th numbers 8 though 15.
+2- "console.log" the 4th value in the array (index3)
+3- "console.log" the whole array.
+*/
+let arr = [8, 9, 10, 11, 12, 13, 14, 15];
+
+console.log(arr[3]);
+console.log(arr);
