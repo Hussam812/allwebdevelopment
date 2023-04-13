a2b02b5ed06e639225d234517337d9e09eab5393
Hussam812 <hossamhosnihussien@gmail.com>
2023-04-13 11:42:26 +0200

    sheet10

---
+++ microverseLevelUpExperience/level2/lvl2sh9.js
@@ -0,0 +1,13 @@
+/*
+Excerxise 1
+Please compleate these steps before moving forward
+1- Create a varible A that contains the results of adding 4 and 7.
+2- Create a varible B that contains multiplying 5 and 9.
+3- Create a varible C that contains the result of dividing B by A.
+4-       'console.log' the result to console
+5- the result should be 4.090909..
+*/
+let A = 4 + 7;
+let B = 5 * 9;
+let C = B / A;
+console.log(C);
---
+++ microverseLevelUpExperience/level2/sh10.js
@@ -0,0 +1,15 @@
+let myapples = 5;
+myapples += 10;
+console.log("I have", myapples, "apples");
+
+
+myapples = 10;
+if(myapples === 0){
+    myapples +=5;
+    console.log("Here you go, now you have",
+    myapples, "apples")
+} else{
+    myapples =0;
+    console.log("Thank you! Now you have",
+    myapples, "apples")
+}
