2168c8f272dbea16179ddc5f331fd01e90ddcbc2
Hussam812 <hossamhosnihussien@gmail.com>
2023-04-13 13:12:33 +0200

    sh17

---
+++ microverseLevelUpExperience/level2/sh17.js
@@ -0,0 +1,14 @@
+/* 
+Exercise 4
+1- Create a while loop that counts from 2 to 9.
+*/
+let i = 2;
+while(i < 10){
+    console.log(i);
+    i ++;
+};
+i = 9
+while(i > 1){
+    console.log(i);
+    i --;
+}
